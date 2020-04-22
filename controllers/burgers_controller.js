1180a4245b3a11499f8269bc6daded18c7d64606
Chris <stephens-c2@live.com>
2020-04-21 20:25:49 -0700

    working on it

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,34 +1,22 @@
 const express = require("express");
 
 const router = express.Router();
-
 const burger = require("../models/burger.js");
-
-
 
 router.get("/", function (req, res) {
     burger.all(function (data) {
-        var stfuObject = {
+        var hbsObject = {
             burgers: data
         };
-
-
-
-
-        console.log(stfuObject);
-        res.render("index", stfuObject);
+        console.log(hbsObject);
+        res.render("index", hbsObject);
     });
 });
-
-
 router.post("/",function(req,res){
     burger.insert(req.body.name, function (data) {
         res.json(data);
     })
 });
-
-
-
 router.put("/:id",function(req,res){
     burger.update(req.params.id, function (data) {
         res.json(data);
