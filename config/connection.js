6ef2b468165d18a0f0f5b1cb7282e8d874e0be61
Chris <stephens-c2@live.com>
2020-04-21 21:28:16 -0700

    doing stuff

--- config/connection.js
+++ config/connection.js
@@ -6,11 +6,11 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
   connection = mysql.createConnection({
-    host: "izm96dhhnwr2ieg0.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    host: "localhost",
     port: 3306,
-    user: "a97xf63z6c1jynt4",
-    password: "j4khw7t0rd4x1yek",
-    database: "p3lapg31l9kkc74o"
+    user: "root",
+    password: "ilBS43v3r!",
+    database: "burger_db"
   });
 }
 
